b5d72769200300a201e06ec87f09a8fd15916105
GlebKasyak <gkebkasyak@gmail.com>
2020-07-15 17:02:02 +0300

    refactor/refactor and add some interfaces

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, Dispatch, SetStateAction } from "react";
 import { NavigationStackOptions, NavigationStackScreenProps } from "react-navigation-stack";
 import { NavigationBottomTabOptions, NavigationTabScreenProps } from "react-navigation-tabs";
 import { NavigationDrawerOptions, NavigationDrawerScreenProps } from "react-navigation-drawer";
@@ -16,3 +16,6 @@ export type DefaultNavigationOptionsType<Props> = Omit<Props, "screenProps"> & {
 export type NavigationStackProps<P> = FC<P & DefaultNavigationOptionsType<NavigationStackScreenProps>> & { navigationOptions?: NavigationStackOptions | ((props: DefaultNavigationOptionsType<NavigationStackScreenProps>) => NavigationStackOptions) };
 export type NavigationTabProps<P> = FC<P & NavigationTabScreenProps> & { navigationOptions?: NavigationBottomTabOptions | ((props: NavigationTabScreenProps) => NavigationBottomTabOptions) };
 export type NavigationDrawerProps<P> = FC<P & NavigationDrawerScreenProps> & { navigationOptions?: NavigationDrawerOptions | ((props: NavigationDrawerScreenProps) => NavigationDrawerOptions) };
+
+
+export type SetStateType<T> = Dispatch<SetStateAction<T>>;
