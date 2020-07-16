7bf04dfee10e1a55f5e8139a6a16ebc617c0b7c8
GlebKasyak <gkebkasyak@gmail.com>
2020-07-16 18:42:20 +0300

    add/add methods for sort markets by time and sort by current distance

--- src/screens/MapScreen/MapScreenContainer.tsx
+++ src/screens/MapScreen/MapScreenContainer.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import MapScreen from "./MapScreen";
 import { Container } from "../../components/atoms";
 
 import { NavigationStackProps } from "../../interfaces/common";
-import markets from "../../shared/markets";
+import { addDistanceToMarkets, getCurrentTime } from "../../shared/helpers";
+import defaultMarkets from "../../shared/markets";
+import { MarketType } from "../../interfaces/market";
 
 import { UserSelectors } from "../../store/selectors";
 import { AppStateType } from "../../store/reducers";
@@ -15,6 +17,42 @@ const MapScreenContainer: NavigationStackProps<{}> = ({ screenProps }) => {
 
     const [isMapReady, serIsMapReady] = useState(false);
     const [circleRadius, setCircleRadius] = useState(0);
+    const [isEnabled, setIsEnabled] = useState(false);
+    const [markets, setMarkets] = useState<Array<MarketType>>([]);
+
+    useEffect(() => {
+        const data = addDistanceToMarkets(defaultMarkets, { latitude, longitude }) as Array<MarketType>;
+        setMarkets(data)
+    },[latitude, longitude, defaultMarkets]);
+
+    useEffect(() => {
+        if(isEnabled) {
+            setMarkets(markets.filter(market => {
+                if(market.openingTime < getCurrentTime() && getCurrentTime() < market.closingTime) {
+                    return market
+                }
+            }));
+        }
+    }, [isEnabled]);
+
+    useEffect(() => {
+        if(circleRadius) {
+            const data = markets.map(market => {
+                if(circleRadius >= market.distance) {
+                    return {
+                        ...market,
+                        isVisible: true
+                    }
+                } else {
+                   return {
+                       ...market,
+                       isVisible: false
+                   }
+                }
+            });
+            setMarkets(data);
+        }
+    }, [circleRadius]);
 
     return (
         <Container style={{ flex: 1, padding: 0 }} >
@@ -26,7 +64,10 @@ const MapScreenContainer: NavigationStackProps<{}> = ({ screenProps }) => {
                 circleRadius={ circleRadius }
                 onRadiusChange={ (value: number) => setCircleRadius(value) }
                 markets={ markets }
+                isEnabled={ isEnabled }
+                onToggle={ () => setIsEnabled(prevValue => !prevValue) }
                 theme={ screenProps.theme }
+                themeName={ screenProps.themeName }
             />
         </Container>
     )
